a55264c1fbc59119bf6b5d6f60960cd85d680dc9
Simone <simonesamardzhiev@gmail.com>
2025-01-09 22:52:58 +0200

    Add utils package

---
+++ utils/response.go
@@ -0,0 +1,18 @@
+package utils
+
+import "net/http"
+
+// Response is a struct used to respond to a http request.
+type Response[T any] struct {
+	// The status code of the response.
+	StatusCode int
+	// The data of the response.
+	Data T
+}
+
+var (
+	InternalServerErrorResponse = Response[string]{
+		StatusCode: http.StatusInternalServerError,
+		Data:       "Internal Server Error",
+	}
+)
