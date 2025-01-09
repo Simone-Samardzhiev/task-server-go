0fd242e843fd63ab24f387c004b3780a2e193ae7
Simone <simonesamardzhiev@gmail.com>
2025-01-09 22:23:35 +0200

    Add repository.go to user package

---
+++ user/repository.go
@@ -0,0 +1,54 @@
+package user
+
+import (
+	"database/sql"
+	"github.com/google/uuid"
+)
+
+// Repository is interface that will manage user data.
+type Repository interface {
+	// CheckUserEmail will check if the user email is in use.
+	CheckUserEmail(email string) (bool, error)
+
+	// AddUser will add a new user to the database.
+	AddUser(user User) error
+
+	// GetUserByEmail will get user by its email.
+	GetUserByEmail(email string) (User, error)
+
+	// DeleteUser will delete user by its id.
+	DeleteUser(id uuid.UUID) error
+}
+
+type PostgresRepository struct {
+	database *sql.DB
+}
+
+func (p *PostgresRepository) CheckUserEmail(email string) (bool, error) {
+	row := p.database.QueryRow("SELECT COUNT(id) FROM users WHERE email = ?", email)
+	var count int
+
+	err := row.Scan(&count)
+	if err != nil {
+		return false, err
+	}
+
+	return count > 0, nil
+}
+
+func (p *PostgresRepository) AddUser(user User) error {
+	_, err := p.database.Exec("INSERT INTO users (id, email, password) VALUES (?, ?, ?)", user.Id, user.Email, user.Password)
+	return err
+}
+
+func (p *PostgresRepository) GetUserByEmail(email string) (User, error) {
+	row := p.database.QueryRow("SELECT * FROM users WHERE email = ?", email)
+	var user User
+	err := row.Scan(&user.Id, &user.Email, &user.Password)
+	return user, err
+}
+
+func (p *PostgresRepository) DeleteUser(id uuid.UUID) error {
+	_, err := p.database.Exec("DELETE FROM users WHERE id = ?", id)
+	return err
+}
--- user/types.go
+++ user/types.go
@@ -1,7 +1,18 @@
 package user
 
-// User is struct used to decode user data from requests.
+import "github.com/google/uuid"
+
+// User is struct that defines used data.
 type User struct {
+	Id uuid.UUID
+	// The email of the user.
+	Email string
+	// The password of the user.
+	Password string
+}
+
+// NewUser is a struct used to decode user information from requests.
+type NewUser struct {
 	// The email of the user.
 	Email string `json:"email"`
 	// The password of the user.
