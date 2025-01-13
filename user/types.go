73850b040b9f070c417edf69f2d6f68e51fa87b5
Simone <simonesamardzhiev@gmail.com>
2025-01-13 22:32:36 +0200

    Add constructors in types.go in user package

--- user/types.go
+++ user/types.go
@@ -11,10 +11,21 @@ type User struct {
 	Password string
 }
 
+func NewUser(email string, id *uuid.UUID, password string) *User {
+	return &User{Email: email, Id: *id, Password: password}
+}
+
 // JsonUser is a struct used to decode user information from requests.
 type JsonUser struct {
 	// The email of the user.
 	Email string `json:"email"`
 	// The password of the user.
 	Password string `json:"password"`
+}
+
+func NewJsonUser(email string, password string) *JsonUser {
+	return &JsonUser{
+		Email:    email,
+		Password: password,
+	}
 }
