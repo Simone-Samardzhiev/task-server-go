72b4e6083d0d2162e5bbf39a872169830a597094
Simone <simonesamardzhiev@gmail.com>
2025-01-13 23:24:40 +0200

    Moved and fix a bug in database.go where the arguments for connection string where wrongly placed

--- database/database.go
+++ database/database.go
@@ -7,7 +7,7 @@ import (
 
 // CreateConnection will create a database connection by using user, password, host and database name.
 func CreateConnection(user, password, host, name string) (*sql.DB, error) {
-	connStr := fmt.Sprintf("user=%s password=%s host=%s dbname=%s sslmode=disable", user, host, password, name)
+	connStr := fmt.Sprintf("user=%s password=%s host=%s dbname=%s sslmode=disable", user, password, host, name)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
