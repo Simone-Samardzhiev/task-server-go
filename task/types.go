f92a71c88c3c7779d6e6de2d080c84c3d197280e
Simone <simonesamardzhiev@gmail.com>
2025-01-16 10:05:05 +0200

    Modify types.go in task package

--- task/types.go
+++ task/types.go
@@ -7,27 +7,13 @@ import (
 
 // Task type struct hold information about a task.
 type Task struct {
-	Id            uuid.UUID  `json:"id"`
-	Name          string     `json:"name"`
-	Description   string     `json:"description"`
-	Type          int        `json:"type"`
-	DueDate       time.Time  `json:"due_date"`
-	DateCompleted *time.Time `json:"date_completed"`
-	DateDeleted   *time.Time `json:"date_deleted"`
-	UserId        uuid.UUID
-}
-
-// NewTask will create a [Task]
-func NewTask(id uuid.UUID, Type int, description string, name string, dueDate time.Time, dateDeleted *time.Time, dateCompleted *time.Time) *Task {
-	return &Task{
-		Id:            id,
-		Type:          Type,
-		Description:   description,
-		Name:          name,
-		DueDate:       dueDate,
-		DateDeleted:   dateDeleted,
-		DateCompleted: dateCompleted,
-	}
+	Id            uuid.UUID `json:"id"`
+	Name          string    `json:"name"`
+	Description   string    `json:"description"`
+	Type          int       `json:"type"`
+	DueDate       time.Time `json:"due_date"`
+	DateCompleted NullTime  `json:"date_completed"`
+	DateDeleted   NullTime  `json:"date_deleted"`
 }
 
 // DataTask is used receive new task from http request.
