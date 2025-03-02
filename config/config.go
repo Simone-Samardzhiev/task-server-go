7a623dbb7ea8537829d1a9e8f1c228f526722529
Simone <simonesamardzhiev@gmail.com>
2025-03-02 19:21:28 +0200

    Add config.go used to load configuration for the app

---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"github.com/joho/godotenv"
+	"os"
+)
+
+// Config struct hold all the configuration of the server.
+type Config struct {
+	ServerAddr string
+}
+
+// NewConfig function will load environment variables and return them as [Config] struct.
+func NewConfig() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		ServerAddr: getEnv("SERVER_ADDR", ":8080"),
+	}, nil
+}
+
+// getEnv will return environment variable with a key.
+// If the variable is not found it will return the fallback.
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
