bd49b5d767b15d087507faf21572823e48aa880d
Simone <simonesamardzhiev@gmail.com>
2025-03-03 14:22:57 +0200

    Chang the first migration to make email and username unique and not null and password to not null

--- migrations/000001_create_user.up.sql
+++ migrations/000001_create_user.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id       SERIAL PRIMARY KEY,
-    email    VARCHAR(255),
-    username VARCHAR(255),
-    password VARCHAR(255)
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
