e0785722213d29d3fe9a22dce666455cc9844535
Simone <simonesamardzhiev@gmail.com>
2025-03-03 16:07:17 +0200

    Make the package db called database

--- database/db.go
+++ database/db.go
@@ -1,12 +1,12 @@
-package db
+package database
 
 import (
 	"database/sql"
 	"server/config"
 )
 
-// connection will connect to the database.
-func connect(conf *config.DatabaseConfig) (*sql.DB, error) {
+// Connect will connect to the database.
+func Connect(conf *config.DatabaseConfig) (*sql.DB, error) {
 	db, err := sql.Open("postgres", conf.Url)
 	if err != nil {
 		return nil, err
