cf11840b62e0ea4acda142fde7bac0e028d21cd5
Simone <simonesamardzhiev@gmail.com>
2025-03-03 12:47:13 +0200

    Modify validate.go to use pointer as return instead of bool variable. Also made it encode to json so the responses can be standardized.

--- utils/validate.go
+++ utils/validate.go
@@ -4,18 +4,16 @@ import "net/http"
 
 // ValidateResponse is a response returned when the payload is empty.
 type ValidateResponse struct {
-	// Variable used to determine if the response is valid.
-	IsValid bool
 	// The message that should be returned
-	Message string
+	Message string `json:"message"`
 	// The status code of the error.
-	StatusCode int
+	StatusCode int `json:"status_code"`
 }
 
 // Payload is an interface used to validate a payload.
 type Payload interface {
 	// ValidatePayload is the method that will validate the payload.
-	ValidatePayload() ValidateResponse
+	ValidatePayload() *ValidateResponse
 }
 
 // HandlePayload will take the check the payload.
@@ -23,7 +21,7 @@ type Payload interface {
 // It returns true if the handler have responded and false if the payload is valid
 func HandlePayload(r *http.Request, w http.ResponseWriter, payload Payload) bool {
 	response := payload.ValidatePayload()
-	if !response.IsValid {
+	if response != nil {
 		w.WriteHeader(response.StatusCode)
 		_, err := w.Write([]byte(response.Message))
 		if err != nil {
