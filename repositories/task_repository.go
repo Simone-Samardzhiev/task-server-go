47b2d23b15b84def614ef5b2c1e090e9b3daeb88
Simone <simonesamardzhiev@gmail.com>
2025-03-04 22:04:24 +0200

    Add task_repository.go to manage tokens data

---
+++ repositories/task_repository.go
@@ -0,0 +1,62 @@
+package repositories
+
+import (
+	"context"
+	"database/sql"
+	"github.com/google/uuid"
+	"time"
+)
+
+// TokenRepository interface managers the tokens data.
+type TokenRepository interface {
+	// AddToken will add a new token.
+	AddToken(ctx context.Context, tokenId uuid.UUID, exp time.Time, userId uuid.UUID) error
+
+	// DeleteToken will delete a token by its id.
+	DeleteToken(ctx context.Context, tokenId uuid.UUID) error
+
+	// CheckToken will search the token id the database and return its subject - The user id.
+	CheckToken(ctx context.Context, tokenId uuid.UUID) (uuid.UUID, error)
+}
+
+type PostgresTokenRepository struct {
+	db *sql.DB
+}
+
+func (r *PostgresTokenRepository) AddToken(ctx context.Context, tokenId uuid.UUID, exp time.Time, userId uuid.UUID) error {
+	_, err := r.db.ExecContext(
+		ctx,
+		`INSERT INTO tokens (id, exp, user_id)
+		VALUES ($1, $2, $3)`,
+		tokenId, exp, userId,
+	)
+
+	return err
+}
+
+func (r *PostgresTokenRepository) DeleteToken(ctx context.Context, tokenId uuid.UUID) error {
+	_, err := r.db.ExecContext(
+		ctx,
+		`DELETE FROM tokens
+       WHERE id = $1`,
+		tokenId,
+	)
+
+	return err
+}
+
+func (r *PostgresTokenRepository) CheckToken(ctx context.Context, tokenId uuid.UUID) (uuid.UUID, error) {
+	row := r.db.QueryRowContext(
+		ctx,
+		`SELECT user_id FROM tokens
+               WHERE id = $1`,
+		tokenId,
+	)
+
+	var userId uuid.UUID
+	err := row.Scan(&userId)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return userId, nil
+}
